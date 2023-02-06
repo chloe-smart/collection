29956cff8666648b3a9dc3330a1d80dcc4c7cc82
Chloe Smart <chloesmartprint@hotmail.com>
2023-02-06 16:10:24 +0000

    added files

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Chloe's Collector App</title>
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
+</head>
+<body>
+    <h1>ARTISTS</h1>
+</body>
+</html>
