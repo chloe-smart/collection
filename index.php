f6ff4a703f34a4edc359fe5781657b60686f645d
Chloe Smart <chloesmartprint@hotmail.com>
2023-02-06 16:30:50 +0000

    changed styling and added html

--- index.php
+++ index.php
@@ -6,9 +6,16 @@
 <html>
 <head>
     <title>Chloe's Collector App</title>
+    <link rel="stylesheet" href="https://use.typekit.net/rkk2hbc.css">
     <link rel="stylesheet" type="text/css" href="styles/style.css">
 </head>
+<header>
+<h1>ARTISTS</h1>
+</header>
 <body>
-    <h1>ARTISTS</h1>
+    <p>Name:</p>
+    <p>Favourite Medium:</p>
+    <p>Known For:</p>
+    <p>Place Of Birth:</p>
 </body>
 </html>
