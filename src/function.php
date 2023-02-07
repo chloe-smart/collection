273c496407af83492e6e47f30b2cc31a9d6558a5
Chloe Smart <chloesmartprint@hotmail.com>
2023-02-07 11:44:21 +0000

    made it so it concatenates all db items, and added media queries

--- src/function.php
+++ src/function.php
@@ -2,11 +2,14 @@
 
 
 function generateArtistHtml($artists) {
+    $artistsOutput ="";
     foreach($artists as $artist){
-    return '<h2>' . $artist['name'] . '</h2>' . 
+    $artistsOutput .='<h2>' . $artist['name'] . '</h2>' . 
     '<p>' . $artist['yearsLived'] . '</p>' .
+    '<img src=' . $artist['image'] . '>' .
     '<p>Favourite Medium: ' . $artist['favouriteMedium'] . '</p>' . 
     '<p>Known For: ' . $artist['knownFor'] . '</p>' . 
     '<p>Place Of Birth: ' . $artist['placeOfBirth'] . '</p>';
     }
+    return $artistsOutput;
 }
